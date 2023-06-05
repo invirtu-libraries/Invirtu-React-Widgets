2c90251c509c366db71ef6b98843cc18a10807ef
Devin Dixon <devindixon@Devins-MacBook-Pro.local>
2023-06-04 22:58:30 -0400

    Updating package.json

---
+++ dist/esm/components/VideoConferencing/VideoConferencing.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import WidgetOptions from '../../interfaces/WidgetOptions';
+declare const VideoConferencing: ({ id, auth_token }: WidgetOptions) => React.JSX.Element;
+export default VideoConferencing;
---
+++ dist/esm/util/useScript.d.ts
@@ -0,0 +1,2 @@
+declare const useScript: (url: string | null, id: string | null) => void;
+export default useScript;
